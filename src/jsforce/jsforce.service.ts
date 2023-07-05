7a7c46468096c70e3a882cd7f26e834d34252fbb
ChandraPrakashRawat <chandra.prakash.rawat@moreyeahs.in>
2023-07-05 11:58:59 +0530

    dynamic login3

--- src/jsforce/jsforce.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Client, InjectJsForce } from '@ntegral/nestjs-force';
-
-@Injectable()
-export class JsforceService {
-    public constructor(@InjectJsForce() private readonly client: Client) {
-        console.log( this.client.conn)
-       }
-}
